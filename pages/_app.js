2972c55aa6a599b8d53fd0c51a04104d1f2f0f48
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-12-17 10:20:07 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,16 @@
-// import "@/styles/globals.css";
-// import React, { useEffect, useState } from "react";
-// export const AuthContext = React.createContext();
-
-// export default function App({ Component, pageProps }) {
-
-//   const contextValue = {
-
-//   };
-
-//   return (
-//     <AuthContext.Provider value={contextValue}>
-//       <Component {...pageProps} />
-//     </AuthContext.Provider>
-//   );
-// }
-
 import "@/styles/globals.css";
-import React, { createContext, useState } from "react";
-
-export const AuthContext = createContext();
+import React, { useEffect, useState } from "react";
+export const AuthContext = React.createContext();
 
 export default function App({ Component, pageProps }) {
-  const [btn1State, setBtn1State] = useState(true);
-  const [btn2State, setBtn2State] = useState(true);
-
-  const toggleBtn1State = () => {
-    setBtn1State(!btn1State);
-  };
-
-  const toggleBtn2State = () => {
-    setBtn2State(!btn2State);
-  };
+  const [btn1, setbtn1] = useState(true);
+  const [btn2, setbtn2] = useState(true);
 
   const contextValue = {
-    btn1State,
-    btn2State,
-    toggleBtn1State,
-    toggleBtn2State,
+    btn1:btn1,
+    btn1:btn2,
+    sebtn1:setbtn1,
+    setbtn2:setbtn2,
   };
 
   return (
